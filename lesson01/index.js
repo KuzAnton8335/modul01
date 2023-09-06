12c84c0f0b4101cb704a0bafd47fd594620b49c6
Kuznecov Anton <ant2783@gmail.com>
2023-09-06 19:24:57 +0300

    lesson01

---
+++ lesson01/index.js
@@ -0,0 +1,10 @@
+'use strict'
+const fruitName = 'nectarine';
+const fruitQuantity = 5;
+const fruitCategory = 'fruits';
+const fruitPrice = 50;
+
+const fruitSum = fruitQuantity * fruitPrice;
+
+console.log(fruitName);
+console.log(fruitSum, 'Руб');
