303b4b602336fcc539a1d4f33b00fe377ff5a286
Kuznecov Anton <ant2783@gmail.com>
2023-09-06 19:35:30 +0300

    new item

--- lesson01/index.js
+++ lesson01/index.js
@@ -1,10 +1,23 @@
 'use strict'
-const fruitName = 'nectarine';
-const fruitQuantity = 5;
-const fruitCategory = 'fruits';
-const fruitPrice = 50;
 
-const fruitSum = fruitQuantity * fruitPrice;
+{
+	const fruitName = 'nectarine';
+	const fruitQuantity = 5;
+	const fruitCategory = 'fruits';
+	const fruitPrice = 50;
 
-console.log(fruitName);
-console.log(fruitSum, 'Руб');
+	const fruitSum = fruitQuantity * fruitPrice;
+
+	console.log(fruitName);
+	console.log(fruitSum, 'Руб');
+}
+
+{
+	const item = 'multimeter';
+	const count = '4';
+	const category = 'прибор для измерения';
+	const price = '2500'
+
+	console.log(item);
+	console.log(`общая сумма товара:${count * price}`);
+}
