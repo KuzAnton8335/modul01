848be267b001e1fb413f245ce807d6f021c75cca
Kuznecov Anton <40388530+KuzAnton8335@users.noreply.github.com>
2023-09-12 13:34:11 +0300

    Update task-04.js
    little s

--- lesson05-1/task-04.js
+++ lesson05-1/task-04.js
@@ -1,5 +1,5 @@
-const calculate = (Summ, orderItems, promoCode) => {
-	let Summ = 0;// Общая сумма корзины
+const calculate = (summ, orderItems, promoCode) => {
+	let summ = 0;// Общая сумма корзины
 	let orderItems = 0; // Количество товаров в корзине
 	let promoCode = ''; // Промокод
 	const oneprocent = 3;
@@ -7,16 +7,16 @@ const calculate = (Summ, orderItems, promoCode) => {
 	const threeprocent = 10;
 
 	if (orderItems >= 10) {
-		const summLite = Summ / 100 * oneprocent;
+		const summLite = summ / 100 * oneprocent;
 		console.log(summLite);
-	} else if (Summ >= 30000) {
-		const summTwo = Summ / 100 * twoprocent;
+	} else if (summ >= 30000) {
+		const summTwo = summ / 100 * twoprocent;
 		console.log(summTwo);
 	} else if (promoCode === "METHED") {
-		const summThree = Summ / 100 * threeprocent;
+		const summThree = summ / 100 * threeprocent;
 		console.log(summThree);
 	} else if (promoCode === "G3H2Z!" || Summ >= 2000) {
-		const summ = Summ / 500;
+		const summ = summ / 500;
 	}
 }
 
