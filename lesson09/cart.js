cb48c7dbc38f16f8aa908377cb20156b885c0d9e
KuzAnton8335 <ant2783@gmail.com>
2023-09-25 15:15:00 +0300

    cart-js add

--- lesson09/cart.js
+++ lesson09/cart.js
@@ -3,10 +3,32 @@ const cart = {
 	totalPrice: 0,
 	count: 0,
 }
+
+const items = 'items';
+let count = 'count';
 const cartPrice = 'totalPrice';
+
 const getTotalPrice = () => {
 	console.log(cart[cartPrice]);
 }
 getTotalPrice();
 
+const calculateItemPrice = () => {
+	console.log(cart[items] * cart[count]);
+}
+calculateItemPrice();
+cart[cartPrice] = calculateItemPrice();
 
+const increaseCount = () => {
+	console.log(cart[count] += 2);
+}
+increaseCount();
+
+const add = {
+	names: 'fruits',
+	price: 0,
+	countAdd: 1
+}
+
+cart.items.push(add);
+console.log(cart);
